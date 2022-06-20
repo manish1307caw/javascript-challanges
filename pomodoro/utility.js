7fcdb8746eb5b768360f167c0fc824a9c1029f1d
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-20 16:12:47 +0530

    Done with adding utility file in the folder

---
+++ pomodoro/utility.js
@@ -0,0 +1,108 @@
+
+import {
+	startButton, secondsRef, minutesRef, checker, settingsButtonRef, ring, zero, interval
+} from './c1.js';
+function setEnabled() {
+	minutesRef.disabled = true;
+	secondsRef.disabled = true;
+	console.log(checker.id);
+	if(checker.id === "true") {
+		checker.id = "false";
+		startButton.innerHTML = "STOP";
+	} else {
+		checker.id = "true";
+		startButton.innerHTML = "START";
+	}
+}
+
+
+function isValid() {
+	minutesRef.value = minutesRef.value.trim();
+	if(minutesRef.value == 0) {
+		minutesRef.value = minutesRef.value[minutesRef.value.length-1];
+	}
+	if(secondsRef.value == 0) {
+		secondsRef.value = secondsRef.value[secondsRef.value.length-1];
+	}
+	if(!(minutesRef.value >= 0 && minutesRef.value < 60) || !(secondsRef.value >= 0 && secondsRef.value < 60)
+		|| minutesRef.value.length === 0 || secondsRef.value.length === 0) {
+		console.log("helllo");
+		return false;
+	} else {
+		console.log("By");
+		
+		return true;
+	}
+}
+
+function invertState(messageToBeDisplayed) {
+	alert(messageToBeDisplayed);
+	setEnabled();
+	minutesRef.value = "00";
+	secondsRef.value = "00";
+	minutesRef.disabled = !minutesRef.disabled;
+	secondsRef.disabled = !secondsRef.disabled;
+}
+
+function changeTiming(interval) {
+	if(secondsRef.value <= 0) {
+		if(minutesRef.value <= 0) {
+			ring.style.stroke = "red";
+			clearInterval(interval);
+			invertState("Time's UP, Enter Value to Start Again And Press Enter");
+
+		} else {
+			secondsRef.value = "59";
+			minutesRef.value--;
+		}
+	} else {
+		secondsRef.value--;
+	}
+	var txt = "".concat(minutesRef.value);
+	if(txt.length === 1) {
+		minutesRef.value = zero.concat(minutesRef.value);
+	}
+	txt = "".concat(secondsRef.value); 
+	if(txt.length === 1) {
+		secondsRef.value = zero.concat(secondsRef.value);
+	}
+}
+
+function alterSettings() {
+	if(checker.id === "true") {
+		// checker.id = "true";
+		ring.style.stroke = "blue";
+		startButton.innerHTML = "START";
+		minutesRef.disabled = false;
+		secondsRef.disabled = false;
+	}
+}
+
+function switcher() {
+	setEnabled();
+	if(checker.id === "false" ){
+		ring.style.stroke = "green";
+		interval = setInterval(function () {
+			if(checker.id === "true"){
+				clearInterval(interval);
+			} else {
+				if(isValid()){
+					changeTiming(interval);
+				} else {
+					ring.style.stroke = "red";
+					clearInterval(interval);
+					invertState("Please Enter a Valid Entry");	
+				}
+			}
+		}, 1000);
+	} else {
+		ring.style.stroke = "blue";
+		console.log("hi");
+	}
+	
+	
+}
+
+export { 
+    setEnabled, isValid, invertState, changeTiming, alterSettings, switcher
+};
