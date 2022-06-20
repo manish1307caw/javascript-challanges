730a5a8227a0c7c744aa4e28d8e366f1d2efda1e
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-20 15:54:59 +0530

    done with adding uitlity function

--- keyboard/app.js
+++ keyboard/app.js
@@ -1,30 +1,33 @@
+import {setJiggleToAnotherKey,
+    keyCheck,
+    preventException} from './utility.js';
 let listOfAllKeysByClassName = document.getElementsByClassName("key");
 
-function setJiggleToAnotherKey() {
-    let keyToBeSelected = Math.floor((Math.random() * 52) + 0);
-    listOfAllKeysByClassName[keyToBeSelected].classList.add("jiggle");
-    listOfAllKeysByClassName[keyToBeSelected].id = "Jiggling";
+// function setJiggleToAnotherKey() {
+//     let keyToBeSelected = Math.floor((Math.random() * 52) + 0);
+//     listOfAllKeysByClassName[keyToBeSelected].classList.add("jiggle");
+//     listOfAllKeysByClassName[keyToBeSelected].id = "Jiggling";
 
-}
-var keyCheck = (keyRef) => {
-    var jigglingElement = document.getElementById("Jiggling");
-    if(jigglingElement.getAttribute("data-key").toString() == keyRef) {
-        jigglingElement.classList.remove("jiggle");
-        jigglingElement.id = "";
-        setJiggleToAnotherKey();
+// }
+// var keyCheck = (keyRef) => {
+//     var jigglingElement = document.getElementById("Jiggling");
+//     if(jigglingElement.getAttribute("data-key").toString() == keyRef) {
+//         jigglingElement.classList.remove("jiggle");
+//         jigglingElement.id = "";
+//         setJiggleToAnotherKey();
         
-    } else {
-        console.log("You Entered Wrong key");
-    }
-}
-function preventException(keyVal){
-    if (keyVal == "TAB") {  //tab pressed
-        event.preventDefault(); // stops its action
-    }
-}
+//     } else {
+//         console.log("You Entered Wrong key");
+//     }
+// }
+// function preventException(event, keyVal){
+//     if (keyVal == "TAB") {  //tab pressed
+//         event.preventDefault(); // stops its action
+//     }
+// }
 window.addEventListener("keydown", (event)=>{
     
     let keyVal = event.key.toString().toUpperCase();
-    preventException(keyVal); 
+    preventException(event, keyVal); 
     keyCheck(keyVal);
 });
---
+++ keyboard/utility.js
@@ -0,0 +1,28 @@
+function setJiggleToAnotherKey() {
+    let keyToBeSelected = Math.floor((Math.random() * 52) + 0);
+    listOfAllKeysByClassName[keyToBeSelected].classList.add("jiggle");
+    listOfAllKeysByClassName[keyToBeSelected].id = "Jiggling";
+
+}
+
+var keyCheck = (keyRef) => {
+    var jigglingElement = document.getElementById("Jiggling");
+    if(jigglingElement.getAttribute("data-key").toString() == keyRef) {
+        jigglingElement.classList.remove("jiggle");
+        jigglingElement.id = "";
+        setJiggleToAnotherKey();
+        
+    } else {
+        console.log("You Entered Wrong key");
+    }
+}
+function preventException(event, keyVal){
+    if (keyVal == "TAB") {  //tab pressed
+        event.preventDefault(); // stops its action
+    }
+}
+export {
+    setJiggleToAnotherKey,
+    keyCheck,
+    preventException
+};
