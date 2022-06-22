29449e2331e137823f834614b78fde92c43661b3
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-22 16:36:15 +0530

    pending will come back to it

--- podcast/app.js
+++ podcast/app.js
@@ -1,3 +1,7 @@
+import {
+  loopThrough, changeStatesOfKey, changeWithoutShift,
+  changeSettingsTillEnd, changeWithShift, changeSettings
+} from './utility.js';
 const checkbox = document.querySelectorAll('input[type="checkbox"]');
 let currentState = "None";
 let currentStateRef = null;
@@ -151,58 +155,7 @@ const episodes = [
     'name': 'Building a Course'
   }
 ];
-let loopThrough = (startPos, endPos, temp) => {
-  for(let i = startPos; i < endPos-1; i++) {
-    checkbox[i].checked = checkbox[temp-1].checked;
-  }
-  
-}
-let changeStatesOfKey = (event) => {
-  let endPos = parseInt(currentStateRef.target.name.slice(8));
-  let startPos = parseInt(event.target.name.slice(8));
-  let temp = startPos;
-  if(endPos < startPos) {
-    temp = startPos;
-    startPos = endPos;
-    endPos = temp;
-  }
-  console.log("hi", startPos, endPos);
-  loopThrough(startPos, endPos, temp);
-}
-let changeWithoutShift = (event) => {
-  // tbi
-  currentStateRef = event;
-  if(event.target.checked) {
-    currentState = "checked";
-  } else{
-    currentState = "unchecked";
-  }
-  console.log("without shift Key", currentStateRef);
-}
-let changeSettingsTillEnd = (event) => {
-  let startPos = parseInt(event.target.name.slice(8));
-  for(let i = startPos; i < 10; i++) {
-    checkbox[i].checked = checkbox[startPos-1].checked;
-  }
-  console.log(startPos);
-}
-let changeWithShift = (event) => {
-  if(currentState === "checked" && event.target.checked || (currentState === "unchecked" && !event.target.checked)) {
-    changeStatesOfKey(event);
-    console.log("within the range")
-  } else {
-    changeSettingsTillEnd(event);
-    console.log("out of range");
-  }
-}
-let changeSettings = (event, element) => {
-  if(event.shiftKey) {
-    changeWithShift(event);
-  } else {
-    changeWithoutShift(event);
-  }
-  console.log(event);
-}
+
 checkbox.forEach((element) => {
   element.addEventListener('click', (event) => {
     changeSettings(event, element);
