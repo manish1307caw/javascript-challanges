6478ae869cc679a34581bafae971e4adf9ad4bbe
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-24 14:25:34 +0530

    Delete keyboard directory

--- keyboard/app.js
+++
@@ -1,7 +0,0 @@
-import { keyCheck, preventException } from "./utility.js";
-
-window.addEventListener("keydown", (event) => {
-  let keyVal = event.key.toString().toUpperCase();
-  preventException(event, keyVal);
-  keyCheck(keyVal);
-});
