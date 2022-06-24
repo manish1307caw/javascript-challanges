e30f40194e1474fa06a5b55b7d4b39af7f421846
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-24 12:33:34 +0530

    done

--- keyboard/app.js
+++ keyboard/app.js
@@ -1,11 +1,12 @@
-import {setJiggleToAnotherKey,
-    keyCheck,
-    preventException} from './utility.js';
-let listOfAllKeysByClassName = document.getElementsByClassName("key");
+import {
+  setJiggleToAnotherKey,
+  keyCheck,
+  preventException
+} from './utility.js';
+let listOfAllKeysByClassName = document.getElementsByClassName('key');
 
-window.addEventListener("keydown", (event)=>{
-    
-    let keyVal = event.key.toString().toUpperCase();
-    preventException(event, keyVal); 
-    keyCheck(keyVal);
+window.addEventListener('keydown', (event) => {
+  let keyVal = event.key.toString().toUpperCase();
+  preventException(event, keyVal);
+  keyCheck(keyVal);
 });
